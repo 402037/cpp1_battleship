a09ba9ed332429006955708cc31c07d5e730d6a1
402037 <147562228+402037@users.noreply.github.com>
2024-01-19 17:44:58 -0600

    Update game.h

--- lib/game.h
+++ lib/game.h
@@ -32,11 +32,35 @@ protected:
     }
 };
 
-class AssignmentGame : public BaseGame {
-  protected:
-    int solve() override {
-        //IMPLEMENT YOUR SEARCH ALGORITHM HERE
-        return SHIP_COUNT;
+class AssignmentGame : public BaseGame 
+{
+protected:
+    //edited the solve() as asked
+    int solve() override
+    {
+        //ships intialized to zero, while board size is initalized to the values of height and width above
+        int ships = 0;
+        int boardsize = HEIGHT + WIDTH;
+
+        //for loop start
+        for (int i = 0; i < boardsize; i++)
+        {
+            //checks if guess function from the board has a hit response type
+            if (this->board->guess(i) == ResponseType::HIT)
+            {
+                //if hit incriment by 1
+                ships++;
+                //checks if ship count id the same as SHIP_COUNT
+                //if it comesback equal it returns to shipcount and loops until all spaces are checked
+                if (ships == SHIP_COUNT)
+                {
+                    //returns if hit
+                    return SHIP_COUNT;
+                }
+            }
+        }
+        //returns the final count once iterated loop goes through
+        return ships;
     }
 };
 
